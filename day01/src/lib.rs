02ae4ca842dbeb31a6187032b096b92adb09d4c1
Pedro de Oliveira <falsovsky@gmail.com>
2024-12-01 16:19:28 +0000

    Better code readability
    - Renamed variables to more meaningful names for better understanding
    - Added detailed comments to clarify code functionality and logic
    - Refactored loops into functional iterations, removing the need for pre-defined result variables

--- day01/src/lib.rs
+++ day01/src/lib.rs
@@ -16,31 +16,43 @@ pub fn read_input(path: PathBuf) -> (Vec<u32>, Vec<u32>) {
 }
 
 pub fn solve_part1(program: &(Vec<u32>, Vec<u32>)) -> u32 {
-    let mut ls = program.0.clone();
-    let mut rs = program.1.clone();
-    ls.sort_unstable();
-    rs.sort_unstable();
-    let mut result: u32 = 0;
-    for (l, r) in ls.into_iter().zip(rs.into_iter()) {
-        result += match l.cmp(&r) {
-            Ordering::Greater => l - r,
-            Ordering::Less => r - l,
-            Ordering::Equal => 0,
-        }
-    }
-    result
+    // Clone vectors to allow sorting
+    let mut left_values = program.0.clone();
+    let mut right_values = program.1.clone();
+
+    // Use sort_unstable() for better performance
+    left_values.sort_unstable();
+    right_values.sort_unstable();
+
+    // Iterate both vectors and calculate the result
+    left_values
+        .iter()
+        .zip(right_values.iter())
+        .fold(0, |result, (&left, &right)| {
+            result
+                + match left.cmp(&right) {
+                    Ordering::Greater => left - right,
+                    Ordering::Less => right - left,
+                    Ordering::Equal => 0,
+                }
+        })
 }
 
 pub fn solve_part2(program: &(Vec<u32>, Vec<u32>)) -> u32 {
+    // Create a similarity score HashMap to avoid multiple computations for
+    // the same number
     let mut similarity = HashMap::new();
-    for &val in &program.1 {
-        *similarity.entry(val).or_insert(0) += 1;
+    for &value in &program.1 {
+        *similarity.entry(value).or_insert(0) += 1;
     }
-    let mut result: u32 = 0;
-    for &l in &program.0 {
-        if let Some(&count) = similarity.get(&l) {
-            result += l * count;
+
+    // Iterate through the left values
+    program.0.iter().fold(0, |result, &left| {
+        // Check if the value exists in the similarity score
+        if let Some(&right_count) = similarity.get(&left) {
+            result + left * right_count
+        } else {
+            result
         }
-    }
-    result
+    })
 }
